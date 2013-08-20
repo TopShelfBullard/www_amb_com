a2b18a6333aadf5ed9cef848bc1bcebb4e2f008a
TopShelfBullard <bullardmitchell@yahoo.com>
2013-08-20 01:47:47 -0500

    finish initial bootstrap configurations

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8" />
-  <title>AMB - home</title>
-</head>
-<body>
+  <head>
+    <title>AMB - home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
 
-  <h1>www.ambullard.com</h1>
-  <p>This is the homepage.</p>
+    <h1>Initial Bootstrap 3 Template</h1>
+    <p>This is the starting point for A. Mitchell Bullard's homepage.</p>
 
-</body>
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+  
+  </body>
 </html>
