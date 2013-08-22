2c5a73b4647b4f7c31982704994e8e275b569426
TopShelfBullard <bullardmitchell@yahoo.com>
2013-08-22 16:10:41 -0500

    solidify hompage layout

--- index.php
+++ index.php
@@ -6,106 +6,134 @@
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
   </head>
-  <body>
+  <body id="body_home">
+    <div class="container">
+
+<!-- heading -->       
+      <div class="heading_row row">
+        <div class="heading_ column col col-lg-4 col-lg-offset-4 col-md-5 col-md-offset-4 col-sm-6 col-sm-offset-3">
+          
+          <h1>A. Mitchell Bullard</h1>
+          
+          <blockquote>
+            developer, writer, musician,
+            <br>
+            all around creativity addict
+          </blockquote>
+
+        </div> <!--  end heading_column -->
+      </div> <!-- end heading_row -->
 
 
-    <div class="row">
-      <div class="main_column col col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
-        <div class="row">
-          <div class="col col-lg-4 col-lg-offset-4">
-            <h1>A. Mitchell Bullard - HOME</h1>
+<!-- navigation bar -->
+      <div class="top_nav navbar hidden-xs">
+        <ul class="nav nav-pills nav-justified">              
+            
+            <li>
+              <a href="#">TECH</a>             
+            </li>
+                             
+            <li>
+              <a href="#">MUSIC</a>             
+            </li>
+              
+            <li>
+              <a href="#">WRITING</a>             
+            </li>
+              
+            <li>
+              <a href="#">BLOG</a>             
+            </li> 
+
+        </ul> 
+      </div> <!-- end top_nav -->
 
 
+<!-- content -->       
         <div class="navbar">
-          <ul class="nav nav-pills">              
-              <li>
-                <a href="#">TECH</a>             
-              </li>
-              
-              <li>
-                <a href="#">MUSIC</a>             
-              </li>
+          <ul class="nav">  
+            <div class="content_row row">
 
-              <li>
-                <a href="#">WRITING</a>             
-              </li>
-              
-              <li>
-                <a href="#">BLOG</a>             
-              </li> 
-
-          </ul> 
-        </div> <!-- top navbar -->
-          </div><!-- heading col -->
-        </div> <!-- heading row -->
-          <div class="navbar">
-            <ul class="nav nav-pills nav-justified">
-            <div class="row">  
-              <div class="tech_column col col-lg-6 col-md-6 col-sm-6">                    
-                <li class="tech_column">
-                  <a href="#"><h1>TECH</h1></a>             
-                  <p>
-                    This is the starting point for A. Mitchell Bullard's tech page. Hello. This is the starting
-                    point for A. Mitchell Bullard's tech page. This is the starting pointb for A. Mitchell
-                    Bullard's tech page. Hi. This is the starting point for A. Mitchell Bullard's tech page.
+<!-- content column one -->
+              <div class="content_column_one col col-lg-5 col-lg-offset-1 col-md-6 col-sm-6 col-xs-6">                    
+                
+                <li class="content_nav">
+                  <a href="#">
+                    <h1>TECH</h1>
+                  </a>             
+                  <img  src="images/tech.png" alt="amb profile pic" width="100%" class="tech_img">
+                  <p class="tech_p1 hidden-xs">
+                    A. Mitchell Bullard studies software development at Chippewa Valley Technical College, and he actively seeks employment in
+                    this field that has become his hobby, his passion... his wife would say, his obsession. He has attended That Conference, Twin
+                    Cities Code Camp, every CVDev Group meeting since he began his studies the winter of 2013, but his favorite IT social activity
+                    just may be, <a href="https://twitter.com/DiabloTuesdays">@DiabdoTuesdays</a>.
                   </p>
-                  <img  src="images/tech.png" alt="amb profile pic" width="100%">
+                  <p class="tech_p2 hidden-xs">
+                    Click TECH to see what A. Mitchell Bullard loves to work with and why he loves it so much.
+                  </p>
                 </li>
-                                <li class="music_column">
-                    <a href="#"><h1>MUSIC</h1></a>
-                    <p>This is the starting point for A. Mitchell Bullard's music page.
-                      Hi. This is the starting point for A. Mitchell Bullard's music page. Hello. This is the starting
-                      point for A. Mitchell Bullard's music page. This is the starting point for A. Mitchell Bullard's
-                      music page.
-                    </p>
-                    <img  src="images/music.png" alt="amb profile pic" width="100%"> 
-                  </li>
-              </div>  
 
+                <li class="content_nav">
+                  <a href="#">
+                    <h1>MUSIC</h1>
+                  </a>
+                  <img  src="images/music.png" alt="amb profile pic" width="100%" class="music_img"> 
+                  <p class="music_p1 hidden-xs">
+                    A. Mitchell Bullard is a graduate of Viterbo University in LaCrosse, WI with a bachelor's degree in music. He has performed in
+                    the Hubert Humphrey Metrodome, upon the Grand Ol' Oprea stage, and at 7th Street Entry in Minneapolis. He has composed
+                    and recorded music for nearly twenty years, during which he left few genres untouched, and he currently performs very well
+                    received concerts for his five children.
+                  </p>
+                  <p class="music_p2 hidden-xs">
+                    Click MUSIC  to see just how funky A. Mitchell Bullard can get with a guitar and a set of vocal chords.
+                  </p>
+                </li>
 
-                <div class="writing_column col col-lg-6 col-md-6 col-sm-6">                  
-                  <li class="writing_column">
-                    <a href="#"><h1>WRITING</h1></a>
-                    <img  src="images/writing.png" alt="amb profile pic" width="100%">
-                    <p>This is the starting point for A. Mitchell Bullard's writing page.
-                      Hello. This is the starting point for A. Mitchell Bullard's writing page. This is the starting
-                      point for A. Mitchell Bullard's writing page. Hi. This is the starting point for A. Mitchell
-                      Bullard's writing page.
-                    </p>           
-                  </li>
-                                  <li class="blog_column">
-                    <a href="#"><h1>BLOG</h1></a>
-                    <img  src="images/profile.png" alt="amb profile pic" width="100%">
-                    <p>This is the starting point for A. Mitchell Bullard's blog page. This
-                      is the starting point for A. Mitchell Bullard's blog page. Hello. This is the starting point for
-                      A. Mitchell Bullard's blog page. This is the starting point for A. Mitchell Bullard's blog page.
-                    </p>
-                  </li>
-                </div>      
+              </div> <!-- end content_column_one -->  
 
-                </div>
+<!-- content column two -->
+              <div class="content_column_two col col-lg-5 col-md-6 col-sm-6 col-xs-6">                  
+                <li class="content_nav">
+                  <a href="#">
+                    <h1>PROSE</h1>
+                  </a>
+                  <p class="prose_p1 hidden-xs">
+                    The first installment A. Mitchell Bullard's explosive fantasy series, "The Many Magical Journeys of Alexander Wright," is currently
+                    available at Amazon's Kindle store. In this epic debut, the line between dreams-come-true and living-nightmares blurs to the point
+                    of indistinction, the love and hate that binds friends and enemies crumbles to dust, and home is replaced by a vast wilderness of
+                    beauty, wonder, and horror.
+                  </p>
+                  <p class="prose_p2 hidden-xs">
+                    Click WRITING to see all of A. Mitchell Bullard's literary endeavors.
+                  </p> 
+                  <img  src="images/writing.png" alt="amb profile pic" width="100%" class="prose_img">      
+                </li>
 
-                </ul>
-              </div> <!-- navbar -->
+                <li class="content_nav">
+                  <a href="#">
+                    <h1>BLOG</h1>
+                  </a>
+                  <p class="blog_p1 hidden-xs">
+                    A. Mitchell Bullard's blog, "&lth1&gt IT Sunset: An IT Student's End of Week Reflections&lt/h1&gt," is place of reflection for him. As an
+                    IT student, every week is a perilous adventure filled with new concepts and and a mountain of new material. He has found that writing
+                    helps the brain organize and process his past to better direct his future.
+                  </p>
+                  <p class="blog_p2 hidden-xs">
+                    Click BLOG for a humorous, candid glimpse into A. Mitchell Bullard's learning process.
+                  </p>                  
+                  <img  src="images/profile.png" alt="amb profile pic" width="100%" class="blog_img">
+                </li>
+              
+              </div> <!-- end content_column_two -->      
 
+            </div> <!-- end content_row -->
+          </ul>
+        </div> <!-- end navbar -->
 
-    
-         
-
-
-
-
-
-
-
-        
-
-      </div> <!-- main_column -->
-    </div> <!-- top_row -->
-        
+    </div> <!-- end container -->
 
     <script src="_/js/bootstrap.js"></script>
     <script src="_/js/myscript.js"></script>
-  
+
   </body>
 </html>
