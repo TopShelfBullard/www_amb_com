bb727676ef9330b520b8838dc6e6555f854462b3
TopShelfBullard <bullardmitchell@yahoo.com>
2013-08-22 02:56:43 -0500

    begin layout

--- index.php
+++ index.php
@@ -8,8 +8,101 @@
   </head>
   <body>
 
-    <h1>Initial Bootstrap 3 Configuration</h1>
-    <p>This is the starting point for A. Mitchell Bullard's homepage.</p>
+
+    <div class="row">
+      <div class="main_column col col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
+        <div class="row">
+          <div class="col col-lg-4 col-lg-offset-4">
+            <h1>A. Mitchell Bullard - HOME</h1>
+
+
+        <div class="navbar">
+          <ul class="nav nav-pills">              
+              <li>
+                <a href="#">TECH</a>             
+              </li>
+              
+              <li>
+                <a href="#">MUSIC</a>             
+              </li>
+
+              <li>
+                <a href="#">WRITING</a>             
+              </li>
+              
+              <li>
+                <a href="#">BLOG</a>             
+              </li> 
+
+          </ul> 
+        </div> <!-- top navbar -->
+          </div><!-- heading col -->
+        </div> <!-- heading row -->
+          <div class="navbar">
+            <ul class="nav nav-pills nav-justified">
+            <div class="row">  
+              <div class="tech_column col col-lg-6 col-md-6 col-sm-6">                    
+                <li class="tech_column">
+                  <a href="#"><h1>TECH</h1></a>             
+                  <p>
+                    This is the starting point for A. Mitchell Bullard's tech page. Hello. This is the starting
+                    point for A. Mitchell Bullard's tech page. This is the starting pointb for A. Mitchell
+                    Bullard's tech page. Hi. This is the starting point for A. Mitchell Bullard's tech page.
+                  </p>
+                  <img  src="images/tech.png" alt="amb profile pic" width="100%">
+                </li>
+                                <li class="music_column">
+                    <a href="#"><h1>MUSIC</h1></a>
+                    <p>This is the starting point for A. Mitchell Bullard's music page.
+                      Hi. This is the starting point for A. Mitchell Bullard's music page. Hello. This is the starting
+                      point for A. Mitchell Bullard's music page. This is the starting point for A. Mitchell Bullard's
+                      music page.
+                    </p>
+                    <img  src="images/music.png" alt="amb profile pic" width="100%"> 
+                  </li>
+              </div>  
+
+
+                <div class="writing_column col col-lg-6 col-md-6 col-sm-6">                  
+                  <li class="writing_column">
+                    <a href="#"><h1>WRITING</h1></a>
+                    <img  src="images/writing.png" alt="amb profile pic" width="100%">
+                    <p>This is the starting point for A. Mitchell Bullard's writing page.
+                      Hello. This is the starting point for A. Mitchell Bullard's writing page. This is the starting
+                      point for A. Mitchell Bullard's writing page. Hi. This is the starting point for A. Mitchell
+                      Bullard's writing page.
+                    </p>           
+                  </li>
+                                  <li class="blog_column">
+                    <a href="#"><h1>BLOG</h1></a>
+                    <img  src="images/profile.png" alt="amb profile pic" width="100%">
+                    <p>This is the starting point for A. Mitchell Bullard's blog page. This
+                      is the starting point for A. Mitchell Bullard's blog page. Hello. This is the starting point for
+                      A. Mitchell Bullard's blog page. This is the starting point for A. Mitchell Bullard's blog page.
+                    </p>
+                  </li>
+                </div>      
+
+                </div>
+
+                </ul>
+              </div> <!-- navbar -->
+
+
+    
+         
+
+
+
+
+
+
+
+        
+
+      </div> <!-- main_column -->
+    </div> <!-- top_row -->
+        
 
     <script src="_/js/bootstrap.js"></script>
     <script src="_/js/myscript.js"></script>
