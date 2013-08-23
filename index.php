f36cbc2b7374aae01ad5537adfd0bb796607c8ef
TopShelfBullard <bullardmitchell@yahoo.com>
2013-08-23 00:32:39 -0500

    adjust image size and refactor styles

--- index.php
+++ index.php
@@ -10,126 +10,136 @@
     <div class="container">
 
 <!-- heading -->       
-      <div class="heading_row row">
+      <section class="heading_row row">
         <div class="heading_ column col col-lg-4 col-lg-offset-4 col-md-5 col-md-offset-4 col-sm-6 col-sm-offset-3">
           
           <h1>A. Mitchell Bullard</h1>
           
-          <blockquote>
+          <p class="heading">
             developer, writer, musician,
             <br>
             all around creativity addict
-          </blockquote>
+          </p>
 
         </div> <!--  end heading_column -->
-      </div> <!-- end heading_row -->
+      </section> <!-- end heading_row -->
 
 
 <!-- navigation bar -->
       <div class="top_nav navbar hidden-xs">
         <ul class="nav nav-pills nav-justified">              
+  
+          <li class="disabled">
+            <a>HOME</a>             
+          </li>
+
+          <li>
+            <a href="#">TECH</a>             
+          </li>
+                           
+          <li>
+            <a href="#">MUSIC</a>             
+          </li>
             
-            <li>
-              <a href="#">TECH</a>             
-            </li>
-                             
-            <li>
-              <a href="#">MUSIC</a>             
-            </li>
-              
-            <li>
-              <a href="#">WRITING</a>             
-            </li>
-              
-            <li>
-              <a href="#">BLOG</a>             
-            </li> 
+          <li>
+            <a href="#">WRITING</a>             
+          </li>
+            
+          <li>
+            <a href="#">BLOG</a>             
+          </li> 
 
         </ul> 
       </div> <!-- end top_nav -->
 
 
-<!-- content -->       
-        <div class="navbar">
-          <ul class="nav">  
-            <div class="content_row row">
+<!-- content -->      
+      <div class=".content_navbar navbar">
+        <ul class="nav">  
 
 <!-- content column one -->
-              <div class="content_column_one col col-lg-5 col-lg-offset-1 col-md-6 col-sm-6 col-xs-6">                    
-                
-                <li class="content_nav">
-                  <a href="#">
-                    <h1>TECH</h1>
-                  </a>             
-                  <img  src="images/tech.png" alt="amb profile pic" width="100%" class="tech_img">
-                  <p class="tech_p1 hidden-xs">
-                    A. Mitchell Bullard studies software development at Chippewa Valley Technical College, and he actively seeks employment in
-                    this field that has become his hobby, his passion... his wife would say, his obsession. He has attended That Conference, Twin
-                    Cities Code Camp, every CVDev Group meeting since he began his studies the winter of 2013, but his favorite IT social activity
-                    just may be, <a href="https://twitter.com/DiabloTuesdays">@DiabdoTuesdays</a>.
-                  </p>
-                  <p class="tech_p2 hidden-xs">
-                    Click TECH to see what A. Mitchell Bullard loves to work with and why he loves it so much.
-                  </p>
-                </li>
+          <section class="left_content_container">
+            <div class="left_content_column col col-lg-6 col-md-6 col-sm-6 col-xs-6">                    
 
-                <li class="content_nav">
-                  <a href="#">
-                    <h1>MUSIC</h1>
-                  </a>
-                  <img  src="images/music.png" alt="amb profile pic" width="100%" class="music_img"> 
-                  <p class="music_p1 hidden-xs">
-                    A. Mitchell Bullard is a graduate of Viterbo University in LaCrosse, WI with a bachelor's degree in music. He has performed in
-                    the Hubert Humphrey Metrodome, upon the Grand Ol' Oprea stage, and at 7th Street Entry in Minneapolis. He has composed
-                    and recorded music for nearly twenty years, during which he left few genres untouched, and he currently performs very well
-                    received concerts for his five children.
-                  </p>
-                  <p class="music_p2 hidden-xs">
-                    Click MUSIC  to see just how funky A. Mitchell Bullard can get with a guitar and a set of vocal chords.
-                  </p>
-                </li>
+              <li class="content_nav">
+                <a href="#">
+                  <h1>TECH</h1>
+                </a>             
+                <img  src="images/tech.png" alt="Developing in the sun on his Mac" width="100%" class="tech_img">
+                <p class="tech_p1 hidden-xs">
+                  A. Mitchell Bullard studies software development at Chippewa Valley Technical College, and he actively seeks employment in
+                  this field that has become his hobby, his passion... his wife would say, his obsession. He has attended 
+                  <a href="http://www.thatconference.com/">That Conference</a>,
+                  <a href="http://www.twincitiescodecamp.com/TCCC/Default.aspx">Twin Cities Code Camp</a>,
+                  every
+                  <a href="http://cvdevgroup.org/">CVDev Group</a>
+                  meeting since he began his studies the winter of 2013, but his favorite IT social activity just may be, 
+                  <a href="https://twitter.com/DiabloTuesdays">@DiabdoTuesdays</a>.
+                </p>
+                <p class="tech_p2 hidden-xs">
+                  Click TECH to see what A. Mitchell Bullard loves to work with and why he loves it so much.
+                </p>
+              </li>
 
-              </div> <!-- end content_column_one -->  
+              <li class="content_nav">
+                <a href="#">
+                  <h1>MUSIC</h1>
+                </a>
+                <img  src="images/music.png" alt="Playing guitar for his children" width="100%" class="music_img"> 
+                <p class="music_p1 hidden-xs">
+                  A. Mitchell Bullard is a graduate of Viterbo University in LaCrosse, WI with a bachelor's degree in music. He has performed in
+                  the Hubert Humphrey Metrodome, upon the Grand Ol' Oprea stage, and at 7th Street Entry in Minneapolis. He has composed
+                  and recorded music for nearly twenty years, during which he left few genres untouched, and he currently performs very well
+                  received concerts for his five children.
+                </p>
+                <p class="music_p2 hidden-xs">
+                  Click MUSIC  to see just how funky A. Mitchell Bullard can get with a guitar and a set of vocal chords.
+                </p>
+              </li>
+
+            </div> <!-- end content_column_one -->  
+          </section> <!-- end left_content_container -->
 
 <!-- content column two -->
-              <div class="content_column_two col col-lg-5 col-md-6 col-sm-6 col-xs-6">                  
-                <li class="content_nav">
-                  <a href="#">
-                    <h1>PROSE</h1>
-                  </a>
-                  <p class="prose_p1 hidden-xs">
-                    The first installment A. Mitchell Bullard's explosive fantasy series, "The Many Magical Journeys of Alexander Wright," is currently
-                    available at Amazon's Kindle store. In this epic debut, the line between dreams-come-true and living-nightmares blurs to the point
-                    of indistinction, the love and hate that binds friends and enemies crumbles to dust, and home is replaced by a vast wilderness of
-                    beauty, wonder, and horror.
-                  </p>
-                  <p class="prose_p2 hidden-xs">
-                    Click WRITING to see all of A. Mitchell Bullard's literary endeavors.
-                  </p> 
-                  <img  src="images/writing.png" alt="amb profile pic" width="100%" class="prose_img">      
-                </li>
+          <section class="right_content_container">
+            <div class="right_content_column col col-lg-6 col-md-6 col-sm-6 col-xs-6">                  
+              <li class="content_nav">
+                <a href="#">
+                  <h1>PROSE</h1>
+                </a>
+                <p class="prose_p1 hidden-xs">
+                  The first installment A. Mitchell Bullard's explosive fantasy series,
+                  <a href="http://www.amazon.com/Mystery-Magical-Journeys-Alexander-ebook/dp/B0083YU4II/ref=cm_sw_em_r_dp_2HUfsb0X00Q24_tt"> The Many Magical Journeys of Alexander Wright</a>,
+                  is currently available at Amazon's Kindle store. In this epic debut, the line between dreams-come-true and living-nightmares
+                  blurs to the point of indistinction, the love and hate that binds friends and enemies crumbles to dust, and home is replaced by a
+                  vast wilderness of beauty, wonder, and horror.
+                </p>
+                <p class="prose_p2 hidden-xs">
+                  Click PROSE to see all of A. Mitchell Bullard's literary endeavors.
+                </p> 
+                <img  src="images/writing.png" alt="Cover image for A. Mitchell Bullard's debut novel" width="100%" class="prose_img">      
+              </li>
 
-                <li class="content_nav">
-                  <a href="#">
-                    <h1>BLOG</h1>
-                  </a>
-                  <p class="blog_p1 hidden-xs">
-                    A. Mitchell Bullard's blog, "&lth1&gt IT Sunset: An IT Student's End of Week Reflections&lt/h1&gt," is place of reflection for him. As an
-                    IT student, every week is a perilous adventure filled with new concepts and and a mountain of new material. He has found that writing
-                    helps the brain organize and process his past to better direct his future.
-                  </p>
-                  <p class="blog_p2 hidden-xs">
-                    Click BLOG for a humorous, candid glimpse into A. Mitchell Bullard's learning process.
-                  </p>                  
-                  <img  src="images/profile.png" alt="amb profile pic" width="100%" class="blog_img">
-                </li>
-              
-              </div> <!-- end content_column_two -->      
+              <li class="content_nav">
+                <a href="#">
+                  <h1>BLOG</h1>
+                </a>
+                <p class="blog_p1 hidden-xs">
+                  A. Mitchell Bullard's blog,
+                  <a href="http://bullardamitchell.wordpress.com/">&lth1&gt IT Sunset: An IT Student's End of Week Reflections &lt/h1&gt</a>,
+                  is place of reflection for him. As an IT student, every week is a perilous adventure filled with new concepts and and a mountain
+                  of new material. He has found that exercise of writing helps his brain organize and process his past to better direct his future.
+                </p>
+                <p class="blog_p2 hidden-xs">
+                  Click BLOG for a humorous, candid glimpse into A. Mitchell Bullard's learning process.
+                </p>                  
+                <img  src="images/profile.png" alt="Profile picture" width="100%" class="blog_img">
+              </li>
 
-            </div> <!-- end content_row -->
-          </ul>
-        </div> <!-- end navbar -->
-
+            </div> <!-- end right_content_column -->      
+          </section> <!-- end right_content_container -->
+        </ul>
+      </div> <!-- end navbar -->
     </div> <!-- end container -->
 
     <script src="_/js/bootstrap.js"></script>
