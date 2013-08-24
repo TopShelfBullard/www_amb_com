b14bc7ca065209ef4ddc5c87547e334da3724679
TopShelfBullard <bullardmitchell@yahoo.com>
2013-08-23 22:30:31 -0500

    add php include for head tag

---
+++ _/components/php/head_tag_contents.php
@@ -0,0 +1,3 @@
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
--- tech.php
+++ tech.php
@@ -2,9 +2,7 @@
 <html lang="en">
   <head>
     <title>AMB - TECH</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
-    <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
+    <?php include '_/components/php/head_tag_contents.php'; ?>
   </head>
   <body id="body_tech">
     <div class="container">
