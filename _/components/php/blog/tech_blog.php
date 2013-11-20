959edebc219ff470b4b8885b14f7e40e3b430fa8
TopShelfBullard <bullardmitchell@yahoo.com>
2013-11-20 12:50:34 -0600

    Change title and add description for tech blog

--- _/components/php/blog/tech_blog.php
+++ _/components/php/blog/tech_blog.php
@@ -1,8 +1,16 @@
 <a href="http://bullardamitchell.wordpress.com/" target="_blank">
-              <h4>&lt;h1&gt;IT Sunset&lt;/h1&gt;</h4>
-            </a> 
+              <h4>puts "blog"</h4>
+            </a>
+            <br> 
             <p>
-              kns paos  fo asp  so sfpo pokasd mg; np gore glkjfn glf df;l df;o gdfpokg df dfsokfdg df;fk 
-              fjd gdfo gfg dfo;lkj sdfojlgk dflkmgn dfkjgu df;odsflgndf pdflkdsf.mng ok ;dfk gsdk lkfn lk df; 
-              lksdfjg lkdfng lsdkfng lfkn glkdfng ldfkmng lk dsfklfl ;ohl;sdfhg l.
+              All things tech are fair game in this geeky student/professional blog. I'll muse upon my own
+              education (formal as well as the hard-knock school of 'Just Jump In and Figure It Out Yourself'),
+              and I will drop advice for other students take or discard.
+            </p>
+            <p>
+              As a new member of the professional programming community, I'll drop some gems for the
+              geek tying to become more employable as well as the veteran employed who has forgotten
+              the awesomeness of what they are paid to do.
             </p> 
+
+            <p>It should be awesome... and code heavy.</p>
