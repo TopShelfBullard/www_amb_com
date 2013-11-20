b99b074d8520103408bc14b9bf212cfcff4fd123
TopShelfBullard <bullardmitchell@yahoo.com>
2013-11-20 13:10:03 -0600

    Add description to appologetics blog and update tech blog

--- _/components/php/blog/apologetics_blog.php
+++ _/components/php/blog/apologetics_blog.php
@@ -1,8 +1,19 @@
 <a href="http://bullardamitchell.wordpress.com/" target="_blank">
-              <h4>Servant of Truth</h4>
+             <h4>Servant of Truth</h4>
             </a> 
+            <br>
             <p>
-              kns paos  fo asp  so sfpo pokasd mg; np gore glkjfn glf df;l df;o gdfpokg df dfsokfdg df;fk 
-              fjd gdfo gfg dfo;lkj sdfojlgk dflkmgn dfkjgu df;odsflgndf pdflkdsf.mng ok ;dfk gsdk lkfn lk df; 
-              lksdfjg lkdfng lsdkfng lfkn glkdfng ldfkmng lk dsfklfl ;ohl;sdfhg l.
-            </p>    
+              Anyone enjoy a good debate?
+            </p>
+            <p>
+              Good, 'cause that's what this blog is all about. If you are a Catholic who wants to see a a fellow
+              member of the Body of Christ defend the faith, an anti-catholic who wants to debate a
+              one of the Catholic faithful in a public forum, or you just like seeing a good conflict, this
+              may just be the blog for you.
+            </p>
+            <p>
+              I will blog the truth, and, if I can find anyone interested, I will post email debates. So put on
+              your gauntlets, gather your well corroborated scientific, historical, and theological facts
+              (logic and common sense are also prerequisites), and come battle with or against the Servant
+              of Truth.
+            </p>                 
--- _/components/php/blog/tech_blog.php
+++ _/components/php/blog/tech_blog.php
@@ -5,11 +5,11 @@
             <p>
               All things tech are fair game in this geeky student/professional blog. I'll muse upon my own
               education (formal as well as the hard-knock school of 'Just Jump In and Figure It Out Yourself'),
-              and I will drop advice for other students take or discard.
+              and I will drop advice for other students to take or discard.
             </p>
             <p>
-              As a new member of the professional programming community, I'll drop some gems for the
-              geek tying to become more employable as well as the veteran employed who has forgotten
+              As a new member of the professional programming community, I'll also drop some gems for
+              the geek tying to become more employable as well as the veteran employed who has forgotten
               the awesomeness of what they are paid to do.
             </p> 
 
