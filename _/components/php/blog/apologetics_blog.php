ff1e2cd959a77cf59d815e9936070c76a0ae4a77
TopShelfBullard <bullardmitchell@yahoo.com>
2013-12-02 00:34:20 -0600

    Fix typo in apologetics blog description

--- _/components/php/blog/apologetics_blog.php
+++ _/components/php/blog/apologetics_blog.php
@@ -5,7 +5,7 @@
               Anyone enjoy a good debate?
             </p>
             <p>
-              Good, 'cause that's what this blog is all about. If you are a Catholic who wants to see a a fellow
+              Good, 'cause that's what this blog is all about. If you are a Catholic who wants to see a fellow
               member of the Body of Christ defend the faith, an anti-catholic who wants to debate a
               one of the Catholic faithful in a public forum, or you just like seeing a good conflict, this
               may just be the blog for you.
