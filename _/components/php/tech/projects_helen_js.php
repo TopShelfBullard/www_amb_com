bb94c07aca66f4f131def1ec00e1e08fd82ec977
TopShelfBullard <bullardmitchell@yahoo.com>
2013-12-02 00:47:02 -0600

    update content for tech project helen.js

--- _/components/php/tech/projects_helen_js.php
+++ _/components/php/tech/projects_helen_js.php
@@ -4,13 +4,15 @@
                     unit about loops with me. Somehow she absolutely understood it. Since she seemed to enjoy programming
                     with me, and since she was able to understand JavaScript loops at nine (when half my class didn't), I've
                     began to teach her a bit of JavaScript. She loved it, began working on a project, and then attended
-                    <a href="http://www.twincitiescodecamp.com/TCCC/Default.aspx" target="blank">Twin Cities Code
-                      Camp 15</a> with me.
+                    <a href="http://www.twincitiescodecamp.com/TCCC/Default.aspx" target="blank">Twin Cities CodeCamp</a>
+                    and
+                    <a href="http://chippewavalleycodecamp.com/" target="blank">Chippewa Valley Code Camp</a>
+                    with me.
                   </p>
                   <p>
                     She had a blast, so we decided to start adding code with daddy to our days. Because life is so hectic for
                     the Bullard family, I have taken to recording screencasts for her (so watch this site for a link to an awesome
                     youtube channel for kids to learn some programming skills), and as long as Helen continues to enjoy code,
-                    we'll keep we'll keep working on it and I guess I'll have a little code camp buddy. Who knows, maybe I'll even
+                    we'll keep working on it and I guess I'll have a little code camp buddy. Who knows, maybe I'll even
                     link some of her work here... Nah, we'll just set her up with a Github account.
                   </p>
