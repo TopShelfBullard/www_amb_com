d656662a21a897b410ec17231a823028fd1c7a29
TopShelfBullard <bullardmitchell@yahoo.com>
2013-12-07 02:37:45 -0600

    alter elements on screencast page

--- screencast.php
+++ screencast.php
@@ -20,7 +20,12 @@
       <section class="content_container  container  col  col-xxs-12">
         <div class="row">
           <section class="content_container  container  col  col-xxs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
-            <h2>The first in a series of programming screencasts for children to be released early January.</h2>
+            <h5>The first in a series of programming screencasts for my daughter is to be released early January.</h5>
+            <br>
+            <p>
+              If you're new to programming, or you're looking for a great spot for you're kid to
+              learn the awesomeness of programming, keep an eye on this page.
+            </p>
           </section>
         </div> <!-- row -->
       </section> <!-- content_container -->
