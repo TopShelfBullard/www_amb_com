c1266f878eca83bef57c7eda9e645639cd6e632d
TopShelfBullard <amb@amitchellbullard.com>
2015-01-06 23:19:14 -0600

    Adds CVCC 7 and splits list into speaker and attendee categories.

--- _/components/php/resume/education_organization.php
+++ _/components/php/resume/education_organization.php
@@ -1,9 +1,22 @@
 
                                 <p><span class="inner_category">Seminars Attended</span><p>
+                               
                                 <ul>
-                                    <li>Twin Cities Code Camp 14</li>
-                                    <li>Twin Cities Code Camp 15</li>
-                                    <li>That Conference 2013</li>
-                                    <li>Chippewa Valley Code Camp 6</li>
-                                    <li>Chippewa Valley Developer's Group</li>
+                                    <li> 
+                                      <p><span class="inner_category">Speaker:</span></p>
+                                      <ul>
+                                        <li>Chippewa Valley Code Camp 7</li>
+                                      </ul>
+                                    </li>
+                                    <li> 
+                                      <p><span class="inner_category">Attendee:</span></p>
+                                      <ul>
+                                        <li>Chippewa Valley Code Camp 6</li>
+                                        <li>Twin Cities Code Camp 14</li>
+                                        <li>Twin Cities Code Camp 15</li>
+                                        <li>That Conference 2013</li>
+                                        <li>Chippewa Valley Developer's Group</li>
+                                      </ul>
+                                    </li>
                                 </ul>
+                                
