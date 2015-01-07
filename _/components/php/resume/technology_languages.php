3628afe1e4de4d9102b4e2958d535eed421effd2
TopShelfBullard <amb@amitchellbullard.com>
2015-01-06 23:42:19 -0600

    Adds Elixir, RSPec, Cucumber, Objective-C, and gh-unit, and removes Bootstrap and PHP.

--- _/components/php/resume/technology_languages.php
+++ _/components/php/resume/technology_languages.php
@@ -1,10 +1,10 @@
 
                                 <p><span class="inner_category">Languages / Frameworks</span></p>
                                 <ul>
+                                    <li>Elixir</li>
+                                    <li>Ruby, Ruby on Rails, RSpec, Cucumber</li>
                                     <li>Java, JUnit</li>
+                                    <li>Objective-C, gh-unit</li>
                                     <li>XML, XSLT, XPath</li>
                                     <li>HTML5, CSS3, LESS, SASS, JavaScript</li>
-                                    <li>Twitter Bootstrap 3</li>
-                                    <li>Ruby, Ruby on Rails, Cucumber</li>
-                                    <li>PHP</li>
                                 </ul>
