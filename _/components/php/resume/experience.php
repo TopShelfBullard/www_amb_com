098db5232b3ccd1ad7ff8d120e84bc738c06bc58
TopShelfBullard <amb@amitchellbullard.com>
2015-01-06 23:31:36 -0600

    Adds Cucumber to professional expirience.

--- _/components/php/resume/experience.php
+++ _/components/php/resume/experience.php
@@ -7,12 +7,14 @@
                                 </p>
 
                                 <ul>
-                                    <li>Developed new features and improved the test coverage for the Altiris Plugin using Java and JUnit.</li>
-                                        <li>Drastically lowered the cyclomatic complexity for the Altiris Plugin.</li>
-                                <li>Developed a web-based assistant in Java for the Altiris Plugin.</li>
-                                <li>Worked on the backlog for the Altiris, SCCM, and JSS-to-JSS plugins, fixing multiple defects in each plugin.</li>
-                                <li>Created manual testing plans for the Altiris and JSS-to-JSS plugins.</li>
-                                <li>Worked closely with a Software QA Engineer to execute and revise the JSS-to-JSS Manual testing plan.</li>
+                                    <li>Developed new features and improved the test coverage for the Altiris plugin using Java and JUnit.</li>
+                                    <li>Drastically lowered the cyclomatic complexity for the Altiris plugin.</li>
+                                    <li>Developed a web-based assistant in Java for the Altiris plugin.</li>
+                                    <li>Worked on the backlog for the Altiris, SCCM, JSS-to-JSS, and SIS Importer plugins, fixing multiple defects in each plugin.</li>
+                                    <li>Created manual testing plans for the Altiris plugins, the SCCM plugin, and Composer.</li>
+                                    <li>Added new features to the Composer app in the Casper Suite, using Objective-C with GHUnit for tests.</li>
+                                    <li>Worked on a large team in one week sprints to provide day one support of OS X Yosemite and iOS 8 for all of JAMF Software products.</li>
+                                    <li>Implemented Cucumber scenarios for the JAMF Binary.</li>
                                 </ul>
 
                                 <p>
@@ -24,7 +26,7 @@
                                 <ul>
                                     <li>Designed, developed, and deployed a website using HTML5, LESS, JavaScript, PHP, and Twitter Bootstrap 3.</li>
                                     <li>Versioned the source code for the project with Git.</li>
-                                    <li>Altered the source code for Twitter Bootstrap 3 to tailor the fluid grid to my site design.</li>
+                                    <li>Redesigned the Twitter Bootstrap 3 fluid grid to fit my needs.</li>
                                 </ul>
 
                                 <p>
