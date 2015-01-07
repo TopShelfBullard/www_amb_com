e084059f1f5d1376ce4fc34dfcdba60404f66617
TopShelfBullard <amb@amitchellbullard.com>
2015-01-06 23:20:03 -0600

    Updates credit and GPA info.

--- _/components/php/resume/education_school.php
+++ _/components/php/resume/education_school.php
@@ -3,8 +3,9 @@
                                 <ul>
                                     <li>Associate Degree&mdash;Software Development
                                     <br>
-                                    Credits: 44/68&mdash;GPA 3.53</li>
+                                    Credits: 59/68&mdash;GPA 3.4</li>
                                 </ul>
+                                <br>
                                 <p class="viterbo"><span class="inner_category">Viterbo University</span></p>
                                 <ul>
                                     <li>B.A. in Music - 2002</li>
