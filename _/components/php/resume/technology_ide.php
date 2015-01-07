fb22086771e9564696062d9aa8a6c64fb605829f
TopShelfBullard <amb@amitchellbullard.com>
2015-01-07 00:09:11 -0600

    Alters the spacing for the source controll section.

--- _/components/php/resume/technology_ide.php
+++ _/components/php/resume/technology_ide.php
@@ -1,7 +1,6 @@
 
                             <p><span class="inner_category">Version Control / IDEs</span></p>
                             <ul>
-                                <li>Git</li>
-                                <li>Subversion</li>
+                                <li>Git, Subversion</li>
                                 <li>Vim, IntelliJ IDEA, Eclipse, Sublime Text</li>
                             </ul>
