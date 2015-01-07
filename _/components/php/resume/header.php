1239cdf0ff002e787279d674b380af49f1204b64
TopShelfBullard <amb@amitchellbullard.com>
2015-01-06 22:50:44 -0600

    Add a break in the contact section of the hearder.

--- _/components/php/resume/header.php
+++ _/components/php/resume/header.php
@@ -5,8 +5,11 @@
                                     </section>
 
                                     <section id="resume_header_right" class="resume_header_right col col-xxs-12 col-sm-6">
-                                        <p>1501 Miles St<br>
-                                        Chippewa Falls, WI 54729
+                                        <p>
+                                            1501 Miles St
+                                            <br>
+                                            Chippewa Falls, WI 54729
+                                            <br>
                                             <a class="resume_header_right" href="mailto:amb@amitchellbullard.com" target="_blank">amb@amitchellbullard.com</a>
                                             <br>
                                             715-226-1492
